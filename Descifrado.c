d6d8d71cdc9ef1a8a144f54b1d687bf6785953ef
dqnid <dani.heras@hotmail.com>
2022-02-28 22:56:01 +0100

    Comentado el posible sistema de generación aleatoria

--- Descifrado.c
+++ Descifrado.c
@@ -1,23 +1,31 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <time.h>
+#include <math.h>
 #include <mpi.h>
 
-#define CHAR_NF		32
-#define CHAR_MAX 	127
-#define CHAR_MIN 	33
-#define ES			0
-#define COMPROBADOR 1
-#define GENERADOR	2
-#define TAG_TIPO	10
-#define TAG_CLARO	11
-#define TAG_LONG	12
-#define TAG_COMP	13
+#define CHAR_NF			32
+#define CHAR_MAX 		127
+#define CHAR_MIN 		33
+#define PESO_COMPROBAR	5000000
+#define PESO_GENERAR	10000000
+#define ES				0
+#define COMPROBADOR 	1
+#define GENERADOR		2
+#define TAG_TIPO		10
+#define TAG_CLARO		11
+#define TAG_LONG		12
+#define TAG_COMP		13
+#define TAG_CON			14
 
 char *leerPalabra(char *texto);
+void fuerza_espera(unsigned long peso);
 
 /*
  * @param ncomp pista1/0
+ * Dudas:	debo enviar longitud o longitud+1
+ * 			Si hago malloc en 0 y envío esa cadena a un puntero de otro, ¿es la misma zona de memoria y no puedo modificar? 
  * */
 
 int main(int argc, char ** argv)
@@ -86,7 +94,7 @@ int main(int argc, char ** argv)
 				if (i<=ncomp) 
 				{ 
 					printf("\n%d) %s, %ld", id, claro, longitud);
-					MPI_Isend(claro, longitud, MPI_CHAR, i, TAG_CLARO, MPI_COMM_WORLD, &request); 
+					MPI_Isend(claro, longitud+1, MPI_CHAR, i, TAG_CLARO, MPI_COMM_WORLD, &request); 
 				}
 			}
 			//Enviar id comprobador a generador (esto desata el descifrado)
@@ -101,6 +109,7 @@ int main(int argc, char ** argv)
 			}
 			puts("");
 			//Inicio escuchas de descifrado
+			free(claro);
 			break;
 		default:
 			//Espera mensaje de 0 con su tipo, recv bloqueante
@@ -108,9 +117,9 @@ int main(int argc, char ** argv)
 			switch (tipo)
 			{
 				case COMPROBADOR:
-					//Espera palabra bloqueante
+					//Espera longitud y palabra bloqueante
 					MPI_Recv(&longitud, 1, MPI_LONG, ES, TAG_LONG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);	
-					MPI_Recv(claro, longitud, MPI_CHAR, ES, TAG_CLARO, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+					MPI_Recv(claro, longitud+1, MPI_CHAR, ES, TAG_CLARO, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 					//Inicio bucle de escucha
 					break;
 				case GENERADOR:
@@ -119,6 +128,27 @@ int main(int argc, char ** argv)
 					//Espera micomp
 					MPI_Recv(&micomp, 1, MPI_INT, ES, TAG_COMP, MPI_COMM_WORLD, MPI_STATUS_IGNORE);	
 					//Inicio bucle descifrado
+					/*
+					char intento[longitud+1];
+					for (int i=0;i<longitud;i++)
+						intento[i]=CHAR_NF;
+					intento[longitud]='\0';
+					srand(id+time(0));
+					//Recorro una matriz de longitud palabra, si espacio en blanco: rand
+					while (1)
+					{
+						for (int i=0;i<longitud;i++)	
+						{
+							if (intento[i]==CHAR_NF)
+							{
+								intento[i] = (rand()%(CHAR_MAX-CHAR_MIN+1)+CHAR_MIN);//revisar, tengo sueño
+							}
+						}
+						//Espero para dar peso al cálculo
+						fuerza_espera(PESO_GENERAR);
+						MPI_Send(intento, longitud, MPI_CHAR, micomp, TAG_CON, MPI_COMM_WORLD);
+					}
+					*/
 					break;
 			}
 			break;
@@ -139,4 +169,9 @@ char *leerPalabra(char *texto)
 	if (palabra == NULL){ return NULL; }
 	strcpy(palabra,buffer);
 	return palabra;
+}
+
+void fuerza_espera(unsigned long peso)
+{
+	for (unsigned long i=1; i<1*peso; i++) sqrt(i);
 }
